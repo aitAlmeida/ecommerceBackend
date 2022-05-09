f18a51da2438fa939d88280ee81b8e8ee68cb87f
Shado2107 <pd39almeida@outlook.com>
2022-05-09 13:01:14 +0000

    add user list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./app.css";
 import Home from './pages/home/Home';
+import UserList from './pages/userList/UserList';
 
 
 function App() {
@@ -12,7 +13,11 @@ function App() {
       <Topbar/>
         <div className="container">
           <Sidebar/> 
-          <Home/>
+          <Routes>
+            <Route path='/' exact element={<Home/>}/>
+            <Route path='/users' element={<UserList/>}/> 
+          </Routes>
+          
         </div>
     </BrowserRouter>
   );
